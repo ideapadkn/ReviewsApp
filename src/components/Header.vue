8a8e8024fcc4ce06ae1899511b23ac171a6e36f8
Kamron or Камрон <kamrush6429@gmail.com>
2023-08-17 10:40:17 +0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="bg-gray-900">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-center h-[80px]">
+        <h2 class="text-green-500 text-3xl font-bold">Rewiew UI</h2>
+        <nav class="flex gap-8">
+          <router-link class="text-white text-[18px] font-semibold" to="/">Home</router-link>
+          <router-link class="text-white text-[18px] font-semibold" to="/about">About</router-link>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="container mx-auto">
+      <!-- Reviews Form -->
+      <!-- Reviews Stats -->
+      <!-- Reviews List -->
+    </div>
+  </div>
+</template>
